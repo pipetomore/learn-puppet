47e0343c3f3c1ee07df4376ae7c060eeab586b82
Tim Baird <pipetomore@gmail.com>
2014-08-21 15:23:48 -0400

    Add demo_apache module

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,2 +1,7 @@
 #Need to update to use directories instead of import declarations
 import 'nodes.pp'
+ # Define filebucket 'main':
+    filebucket { 'main':
+      server => 'ops1.agile.pragmatics.com',
+      path   => false,
+    }
